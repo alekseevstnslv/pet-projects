bdf0ee41a38ec22a5ee9bd1b36ae456fdbc49ec7
alekseevstnslv <stsv.alekseev@gmail.com>
2025-02-19 10:42:12 +0300

    2025-02-19 Initional commit

---
+++ callculator/main.py
@@ -0,0 +1,19 @@
+from typing import NamedTuple
+from 
+class Person(NamedTuple):
+    name:str
+    current_weigth:float
+    desired_weigth:float
+    height:float
+    gender:bool
+    age:int
+    activity:int
+
+def main:
+    get_current_person_parameters()
+    calculate(Person)
+    print_recomendation()
+    create_pdf_with_result()
+
+if __name__ == "__main__":
+    print("Hi, there!")
